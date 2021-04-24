a0851d65467a2e46c923dc1d856e145ae6eb831b
Kalaiselvan V <kalaiselvan.v@htcindia.com>
2021-04-24 08:11:31 +0530

    State Projects

---
+++ state-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import State from './State.js'
+function App() {
+  return (
+    <div className="App">
+     <State />
+    </div>
+  );
+}
+
+export default App;
---
+++ state-app/src/State.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+export default class State extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            brand:"Ford",
+            model:"Mustang",
+            color:"Red",
+            year: 1964
+        }
+    }
+    changeColor = ()=>{ this.setState({color:"blue"})}
+
+    render(){
+        return (
+            <div>
+            <h1> My {this.state.brand}
+            </h1>
+            <p>
+                It is a {this.state.color}
+                        {this.state.model}
+                from    {this.state.year}        
+            </p>
+            <button type="button" onClick ={this.changeColor}>Change Color</button>
+
+            </div>
+        )
+    }
+}
